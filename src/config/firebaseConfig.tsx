676e146169c133b33549c662c60ef1b5dcdd16e6
Minn <radic7700@gmail.com>
2023-12-28 14:13:41 +0900

    README 수정

---
+++ src/config/firebaseConfig.tsx
@@ -0,0 +1,26 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import firebaseui from "firebaseui";
+// import firebase from "firebase";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBXUB9Fk6VoGlkVghM5Vpw7weCU9Nz1L8s",
+//   authDomain: "react-shoppingmall-app-80c15.firebaseapp.com",
+//   projectId: "react-shoppingmall-app-80c15",
+//   storageBucket: "react-shoppingmall-app-80c15.appspot.com",
+//   messagingSenderId: "311382742781",
+//   appId: "1:311382742781:web:14518de87cfe626bb75094",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// // Initialize the FirebaseUI Widget using Firebase.
+// var ui = new firebaseui.auth.AuthUI(firebaseConfig.auth());
+
+// ui.start("#firebaseui-auth-container", {
+//   signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+//   // Other config options...
+// });
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -3,6 +3,8 @@ import React from "react";
 export default function LoginPage() {
   return (
     <div>
+      <script src="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.js"></script>
+      <link type="text/css" rel="stylesheet" href="https://www.gstatic.com/firebasejs/ui/6.0.1/firebase-ui-auth.css" />
       <div className="flex flex-col items-center border m-20 p-16">
         <div className="text-3xl mb-8">LogIn</div>
         <div>
